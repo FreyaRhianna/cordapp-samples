2dd3afe9aaacea8c50ae210cfe5c7ab938a8bda0
FreyaRhianna <freyarsh@gmail.com>
2019-06-27 16:32:37 +0100

    Playing around with evolvabletokens

---
+++ contracts/src/main/kotlin/com/template/states/ArtWorkTokenType.kt
@@ -0,0 +1,24 @@
+package com.template.states
+
+import com.r3.corda.lib.tokens.contracts.states.EvolvableTokenType
+import com.template.contracts.ArtWorkTokenTypeContract
+import net.corda.core.contracts.Amount
+import net.corda.core.contracts.BelongsToContract
+import net.corda.core.contracts.UniqueIdentifier
+import net.corda.core.identity.Party
+import com.r3.corda.lib.tokens.money.FiatCurrency
+
+@BelongsToContract(ArtWorkTokenTypeContract::class)
+class ArtWorkTokenType (
+        val quality: String,
+        val valuation : Amount<FiatCurrency>,
+        val artist:String,
+        val maintainer: Party,
+        override val fractionDigits: Int = 0,
+        override val linearId: UniqueIdentifier = UniqueIdentifier())
+    : EvolvableTokenType() {
+    companion object {
+        val contractId = this::class.java.enclosingClass.canonicalName
+    }
+    override val maintainers: List<Party> get() = listOf(maintainer)
+}
