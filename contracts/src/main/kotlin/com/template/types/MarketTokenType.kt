8eccfcc1ec61b585eac8ee90b98f0968a9e85b06
Anthony Nixon <anthonynixon@16LHR-MAC31.local>
2019-06-28 11:51:13 +0100

    MarketTokenType now represents a set of digital currencies related to Art

--- contracts/src/main/kotlin/com/template/types/MarketTokenType.kt
+++ contracts/src/main/kotlin/com/template/types/MarketTokenType.kt
@@ -1,8 +1,15 @@
 package com.template.types
 
-import com.r3.corda.lib.tokens.contracts.types.TokenType
+import com.r3.corda.lib.tokens.money.Money
 
-data class MarketTokenType(override val fractionDigits: Int, override val tokenIdentifier: String) : TokenType {
+/*
+Defining a Token for a digital art market - Similar to [DigitalCurrency]
+ */
+data class MarketTokenType(
+        override val tokenIdentifier: String,
+        override val description: String,
+        override val fractionDigits: Int = 0
+) : Money {
     override val tokenClass: Class<*> get() = javaClass
     override fun toString(): String = tokenIdentifier
 
@@ -11,8 +18,9 @@ data class MarketTokenType(override val fractionDigits: Int, override val tokenI
     // token conforms to the correct fractionDigits.
     companion object {
         private val registry = mapOf(
-                Pair("WIBBLE", MarketTokenType(2, "MarketToken")),
-                Pair("WOBBLE", MarketTokenType(5, "MarketToken"))
+                Pair("PXL", MarketTokenType("PXL", "Pixely", 6)),
+                Pair("MUS", MarketTokenType("MUS", "Musicx", 6)),
+                Pair("MME", MarketTokenType("MME", "MultMee", 6))
         )
 
         fun getInstance(code: String): MarketTokenType {
--- workflows/src/main/kotlin/com/template/flows/FlowWithMarketToken.kt
+++ workflows/src/main/kotlin/com/template/flows/FlowWithMarketToken.kt
@@ -6,6 +6,7 @@ import com.r3.corda.lib.tokens.contracts.utilities.issuedBy
 import com.r3.corda.lib.tokens.contracts.utilities.of
 import com.r3.corda.lib.tokens.money.FiatCurrency
 import com.r3.corda.lib.tokens.workflows.flows.shell.IssueTokens
+import com.template.types.MarketTokenType
 import net.corda.core.flows.FlowLogic
 import net.corda.core.flows.StartableByRPC
 import net.corda.core.identity.Party
@@ -16,9 +17,11 @@ import net.corda.core.utilities.ProgressTracker
 class FlowWithMarketToken(val currency: String, val amount: Long, val recipient: Party) : FlowLogic<SignedTransaction>() {
     override val progressTracker = ProgressTracker()
 
+    // FiatCurrency is a token representing off-ledger value
     @Suspendable
     override fun call(): SignedTransaction {
-        val token = FiatCurrency.getInstance(currency)
+        //val token = FiatCurrency.getInstance(currency)
+        val token = MarketTokenType.getInstance("WIBBLE")
         // Starts a new flow session.
         return subFlow(IssueTokens(amount of token issuedBy ourIdentity heldBy recipient))
     }
